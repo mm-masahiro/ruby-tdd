eed9e235cfe65247418403adc58ec2e0e73fe83c
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-07-09 13:41:04 +0900

    test: red

--- spec/qiita/fizz_buzz.rb
+++ spec/qiita/fizz_buzz.rb
@@ -3,12 +3,17 @@ require_relative '../../lib/qiita/fizz_buzz'
 RSpec.describe FizzBuzz do
 	describe "def fizz_buzz" do
 		let(:fizz_buzz) { FizzBuzz.new }
+
 		it "1を引数に与えたら1を返す" do
 			expect(fizz_buzz.fizz_buzz(1)).to eq(1)
 		end
 
 		it "2を引数に与えたら2を返す" do
 			expect(fizz_buzz.fizz_buzz(2)).to eq(2)
+		end
+
+		it "3を引数に与えたら文字列Fizzを返す" do
+			expect(fizz_buzz.fizz_buzz(3)).to eq("Fizz")
 		end
 	end
 end
